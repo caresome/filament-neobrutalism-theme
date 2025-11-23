f32874ee198ec397667f60c3c3b19283cbc03d2f
Caresome <caresome@proton.me>
2025-11-23 14:40:52 +0545

    customization option for css variable

--- src/NeobrutalismeTheme.php
+++ src/NeobrutalismeTheme.php
@@ -2,14 +2,41 @@
 
 namespace Caresome\FilamentNeobrutalism;
 
+use Closure;
 use Filament\Contracts\Plugin;
 use Filament\Panel;
+use Filament\Support\Facades\FilamentAsset;
 
 class NeobrutalismeTheme implements Plugin
 {
+    protected array $customizations = [];
+
     public function getId(): string
     {
         return 'filament-neobrutalism';
+    }
+
+    public function customize(array|Closure $variables): static
+    {
+        $this->customizations[] = $variables;
+
+        return $this;
+    }
+
+    protected function getCustomizations(): array
+    {
+        $variables = [];
+
+        foreach ($this->customizations as $set) {
+            $set = is_callable($set) ? $set() : $set;
+
+            foreach ($set as $key => $value) {
+                $cssVar = str_starts_with($key, '--') ? substr($key, 2) : "neo-{$key}";
+                $variables[$cssVar] = $value;
+            }
+        }
+
+        return $variables;
     }
 
     public function register(Panel $panel): void
@@ -20,7 +47,12 @@ class NeobrutalismeTheme implements Plugin
             ->widgets([]);
     }
 
-    public function boot(Panel $panel): void {}
+    public function boot(Panel $panel): void
+    {
+        if ($customizations = $this->getCustomizations()) {
+            FilamentAsset::registerCssVariables($customizations, 'caresome/filament-neobrutalism-theme');
+        }
+    }
 
     public static function make(): static
     {
